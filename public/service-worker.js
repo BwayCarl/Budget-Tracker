bd57449e300b1fbfc98766989cc88ca5fde80cc2
Carlos <bwaycarl@gmail.com>
2020-10-29 13:13:04 -0400

    Fixed cache names and const.

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,26 +4,28 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-    '/manifest.webmanifest',
     '/index.js',
+    '/service-worker.js',
+    '/manifest.webmanifest',
     '/db.js',
     '/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
   ];
   
-  const STATIC_CACHE = "static-cache-v1";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
   // Install
 self.addEventListener("install", function (evt) {  
   // pre cache all static assets
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(STATIC_CACHE).then((cache) => {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+});
   // Tell the browser to activate this service worker immediately once it has finished installing
   self.skipWaiting();
-});
+
 
 // Activate
 self.addEventListener("activate", function(evt) {
@@ -31,7 +33,7 @@ self.addEventListener("activate", function(evt) {
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          if (key !== STATIC_CACHE && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
           }
@@ -54,7 +56,6 @@ self.addEventListener("fetch", function(evt) {
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
-
             return response;
           })
           .catch(err => {
@@ -67,7 +68,7 @@ self.addEventListener("fetch", function(evt) {
   }
 
   evt.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(STATIC_CACHE).then(cache => {
       return cache.match(evt.request).then(response => {
         return response || fetch(evt.request);
       });
